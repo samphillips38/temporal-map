afdf4d2b54002a6c434b5d783b0a53147dca63db
Sam Phillips <srp60@cam.ac.uk>
2022-11-14 22:54:40 +0000

    Created Map model

--- temporalMapApp/models.py
+++ temporalMapApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Map(models.Model):
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    coords = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return self.title
